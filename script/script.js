276e681fe4c8e0b9400777847ec20be482fcf026
Levron777 <saf2704@gmail.com>
2018-10-26 06:16:13 +0700

    Добавил скролл эффекты

--- script/script.js
+++ script/script.js
@@ -2,157 +2,157 @@
 // Анимация появления заголовков каждого блока
 $(document).ready( function() {
 
-  $(window).scroll(function() {
-    $('.stackH1').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 350 ) {
-        $(this).addClass(' animated bounceInDown slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.stackH1').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 350 ) {
+  //       $(this).addClass(' animated bounceInDown slow');
+  //     }
+  //   });
+  // });
 
-  $(window).scroll(function() {
-    $('.worksH1').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 300 ) {
-        $(this).addClass(' animated fadeInUp slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.worksH1').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 300 ) {
+  //       $(this).addClass(' animated fadeInUp slow');
+  //     }
+  //   });
+  // });
 
-  $(window).scroll(function() {
-    $('.skillsH1').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 300 ) {
-        $(this).addClass(' animated fadeInUp slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.skillsH1').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 300 ) {
+  //       $(this).addClass(' animated fadeInUp slow');
+  //     }
+  //   });
+  // });
 
-  $(window).scroll(function() {
-    $('.contactsH1').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 500 ) {
-        $(this).addClass(' animated fadeInUp slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.contactsH1').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 500 ) {
+  //       $(this).addClass(' animated fadeInUp slow');
+  //     }
+  //   });
+  // });
 
   // Анимация появления блоков front-end back-end
 
-  $(window).scroll(function() {
-    $('.itemStack1').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 500 ) {
-        $(this).addClass(' animated fadeInLeft slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.itemStack1').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 500 ) {
+  //       $(this).addClass(' animated fadeInLeft slow');
+  //     }
+  //   });
+  // });
 
-  $(window).scroll(function() {
-    $('.itemStack2').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 500 ) {
-        $(this).addClass(' animated fadeInRight slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.itemStack2').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 500 ) {
+  //       $(this).addClass(' animated fadeInRight slow');
+  //     }
+  //   });
+  // });
     
   // Анимация появления блоков портфолио
 
-  $(window).scroll(function() {
-    $('.itemWorks1').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 500 ) {
-        $(this).addClass('animated pulse slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.itemWorks1').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 500 ) {
+  //       $(this).addClass('animated pulse slow');
+  //     }
+  //   });
+  // });
 
-  $(window).scroll(function() {
-    $('.itemWorks2').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 500 ) {
-        $(this).addClass('animated pulse slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.itemWorks2').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 500 ) {
+  //       $(this).addClass('animated pulse slow');
+  //     }
+  //   });
+  // });
 
-  $(window).scroll(function() {
-    $('.itemWorks3').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 500 ) {
-        $(this).addClass('animated pulse slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.itemWorks3').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 500 ) {
+  //       $(this).addClass('animated pulse slow');
+  //     }
+  //   });
+  // });
 
-  $(window).scroll(function() {
-    $('.itemWorks4').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 500 ) {
-        $(this).addClass('animated pulse slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.itemWorks4').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 500 ) {
+  //       $(this).addClass('animated pulse slow');
+  //     }
+  //   });
+  // });
 
   // Анимация при наведении на блоки портфолио 
 
-  $('.itemWorks1').hover(
-    function() {
-      $(this).toggleClass(' pulse slow');
-    }
-  );
+  // $('.itemWorks1').hover(
+  //   function() {
+  //     $(this).toggleClass(' pulse slow');
+  //   }
+  // );
 
-  $('.itemWorks2').hover(
-    function() {
-      $(this).toggleClass(' pulse slow');
-    }
-  );
+  // $('.itemWorks2').hover(
+  //   function() {
+  //     $(this).toggleClass(' pulse slow');
+  //   }
+  // );
 
-  $('.itemWorks3').hover(
-    function() {
-      $(this).toggleClass(' pulse slow');
-    }
-  );
+  // $('.itemWorks3').hover(
+  //   function() {
+  //     $(this).toggleClass(' pulse slow');
+  //   }
+  // );
 
-  $('.itemWorks4').hover(
-    function() {
-      $(this).toggleClass(' pulse slow');
-    }
-  );
+  // $('.itemWorks4').hover(
+  //   function() {
+  //     $(this).toggleClass(' pulse slow');
+  //   }
+  // );
 
   // Анимация появления блока Email и ссылки на страницу в ВК
 
-  $(window).scroll(function() {
-    $('.contactsEmail').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 600 ) {
-        $(this).addClass(' animated fadeInLeft slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.contactsEmail').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 600 ) {
+  //       $(this).addClass(' animated fadeInLeft slow');
+  //     }
+  //   });
+  // });
 
-  $(window).scroll(function() {
-    $('.contactsVK').each(function() {
-      var imagePosition = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if ( imagePosition < topOfWindow + 600 ) {
-        $(this).addClass(' animated fadeInRight slow');
-      }
-    });
-  });
+  // $(window).scroll(function() {
+  //   $('.contactsVK').each(function() {
+  //     var imagePosition = $(this).offset().top;
+  //     var topOfWindow = $(window).scrollTop();
+  //     if ( imagePosition < topOfWindow + 600 ) {
+  //       $(this).addClass(' animated fadeInRight slow');
+  //     }
+  //   });
+  // });
 
   // Показать/скрыть Email
 
@@ -183,6 +183,8 @@ $(document).ready( function() {
       $('.scroll-up').fadeOut();
     }
   });
+
+  // Плавная анимация прокрутки 
 
   $(function(){
     $('a[href^="#"]').on('click', function(event) {
